887484a2dd670f5731f3fe273ac7208faad9365c
Rosalv <114425106+cintia84@users.noreply.github.com>
2024-10-04 17:19:16 -0300

    script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function play(){
         bird_dy = bird_dy + grativy;
         document.addEventListener('keydown', (e) => {
             if(e.key == 'ArrowUp' || e.key == ' '){
-                img.src = 'imagenes/Bird-2.png';
+                img.src = 'Bird-2.png';
                 bird_dy = -7.6;
             }
         });
